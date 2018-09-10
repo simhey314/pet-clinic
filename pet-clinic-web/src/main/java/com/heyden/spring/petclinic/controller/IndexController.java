2979d6eca61f3aade39bbf757f0857ba666d3a50
Simon Heyden <simon@family-heyden.net>
2018-09-11 01:39:26 +0200

    implements index controller and html
    - add the index controller
    - add the index html
    - Closes #8

---
+++ pet-clinic-web/src/main/java/com/heyden/spring/petclinic/controller/IndexController.java
@@ -0,0 +1,15 @@
+package com.heyden.spring.petclinic.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping
+public class IndexController {
+
+	@GetMapping({"", "/", "index", "index.html"})
+	public String index() {
+		return "index";
+	}
+}
