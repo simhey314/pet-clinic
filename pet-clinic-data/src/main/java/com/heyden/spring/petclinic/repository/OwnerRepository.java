21e71f48ec0c9cfa10e4a343b2ed93f9fe115d50
Simon Heyden <simon@family-heyden.net>
2018-09-14 22:02:15 +0200

    add jpa owner service
    - add OwnerJPAService.java with profile jpa
    - Closes #40

--- pet-clinic-data/src/main/java/com/heyden/spring/petclinic/repository/OwnerRepository.java
+++ pet-clinic-data/src/main/java/com/heyden/spring/petclinic/repository/OwnerRepository.java
@@ -4,4 +4,6 @@ import com.heyden.spring.petclinic.entity.Owner;
 import org.springframework.data.repository.CrudRepository;
 
 public interface OwnerRepository extends CrudRepository<Owner, Long> {
+
+	Owner findByLastName(String lastName);
 }
