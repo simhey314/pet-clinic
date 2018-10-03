cd6cc63aae97a41d2ffe7d800c7b1ab1fb816427
Simon Heyden <simon@family-heyden.net>
2018-10-03 15:24:20 +0200

    junit test for owner jpa service
    - Closes #50

--- pet-clinic-data/src/main/java/com/heyden/spring/petclinic/service/jpa/OwnerJPAService.java
+++ pet-clinic-data/src/main/java/com/heyden/spring/petclinic/service/jpa/OwnerJPAService.java
@@ -2,6 +2,8 @@ package com.heyden.spring.petclinic.service.jpa;
 
 import com.heyden.spring.petclinic.entity.Owner;
 import com.heyden.spring.petclinic.repository.OwnerRepository;
+import com.heyden.spring.petclinic.repository.PetRepository;
+import com.heyden.spring.petclinic.repository.PetTypeRepository;
 import com.heyden.spring.petclinic.service.OwnerService;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
@@ -10,12 +12,17 @@ import org.springframework.stereotype.Service;
 @Profile("jpa")
 public class OwnerJPAService extends AbstractJPAService<Owner, Long, OwnerRepository> implements OwnerService {
 
-	public OwnerJPAService(final OwnerRepository ownerRepository) {
+	private PetTypeRepository petTypeRepository;
+	private PetRepository petRepository;
+
+	public OwnerJPAService(OwnerRepository ownerRepository, PetTypeRepository petTypeRepository, PetRepository petRepository) {
 		super(ownerRepository);
+		this.petTypeRepository = petTypeRepository;
+		this.petRepository = petRepository;
 	}
 
 	@Override
 	public Owner findByLastName(final String lastName) {
-		return repository.findByLastName(lastName);
+		return repository.findByLastNameLike(lastName);
 	}
 }
---
+++ pet-clinic-data/src/test/java/com/heyden/spring/petclinic/service/jpa/OwnerJPAServiceTest.java
@@ -0,0 +1,69 @@
+package com.heyden.spring.petclinic.service.jpa;
+
+import com.heyden.spring.petclinic.entity.Owner;
+import com.heyden.spring.petclinic.repository.OwnerRepository;
+import com.heyden.spring.petclinic.repository.PetRepository;
+import com.heyden.spring.petclinic.repository.PetTypeRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class OwnerJPAServiceTest {
+
+	public static final String LAST_NAME = "lastname";
+
+	@Mock
+	OwnerRepository ownerRepository;
+	@Mock
+	PetTypeRepository petTypeRepository;
+	@Mock
+	PetRepository petRepository;
+
+	@InjectMocks
+	OwnerJPAService underTest;
+	private Owner expectedOwner;
+
+	private void setup() {
+		expectedOwner = new Owner();
+		expectedOwner.setId(1L);
+		expectedOwner.setLastName(LAST_NAME);
+	}
+
+	@Test
+	void findByLastName() {
+
+		when(ownerRepository.findByLastNameLike(anyString())).thenReturn(expectedOwner);
+
+		Owner actual = underTest.findByLastName(LAST_NAME);
+
+		assertThat(actual.getLastName()).isEqualTo(LAST_NAME);
+		verify(ownerRepository, times(1)).findByLastNameLike(anyString());
+	}
+
+	@Test
+	void findAll() {
+	}
+
+	@Test
+	void findById() {
+	}
+
+	@Test
+	void save() {
+	}
+
+	@Test
+	void delete() {
+	}
+
+	@Test
+	void deleteById() {
+	}
+}
