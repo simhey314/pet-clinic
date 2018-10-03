bcf4155553b69cbb7f849f55151b4401fe56feee
Simon Heyden <simon@family-heyden.net>
2018-10-03 17:00:45 +0200

    add owner detail
    - test
    - view
    - controller
    - Closes #52

--- pet-clinic-web/src/main/java/com/heyden/spring/petclinic/controller/OwnerController.java
+++ pet-clinic-web/src/main/java/com/heyden/spring/petclinic/controller/OwnerController.java
@@ -4,6 +4,7 @@ import com.heyden.spring.petclinic.service.OwnerService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -25,6 +26,12 @@ public class OwnerController {
 	@GetMapping("/find")
 	public String findOwner() {
 		return "notYetImplemented";
+	}
+
+	@GetMapping("/{id}")
+	public String detailOwner(@PathVariable Long id, Model model) {
+		model.addAttribute("owner", ownerService.findById(id));
+		return "owners/details";
 	}
 
 }
--- pet-clinic-web/src/test/java/com/heyden/spring/petclinic/controller/OwnerControllerTest.java
+++ pet-clinic-web/src/test/java/com/heyden/spring/petclinic/controller/OwnerControllerTest.java
@@ -14,7 +14,8 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import java.util.HashSet;
 import java.util.Set;
 
-import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.*;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.verifyZeroInteractions;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -64,6 +65,18 @@ class OwnerControllerTest {
 				.andExpect(status().isOk())
 				.andExpect(view().name("owners/index"))
 				.andExpect(model().attribute("owners", hasSize(2)));
+	}
+
+	@Test
+	void detailOwner() throws Exception {
+		Owner expected = new Owner();
+		expected.setId(123l);
+		when(ownerService.findById(anyLong())).thenReturn(expected);
+
+		mockMVC.perform(get("/owners/123"))
+				.andExpect(status().isOk())
+				.andExpect(view().name("owners/details"))
+				.andExpect(model().attribute("owner", hasProperty("id", is(123l))));
 	}
 
 	@Test
